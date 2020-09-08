490e239fb46c39912de8e0d39b6c585c5b426083
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-09-08 20:11:24 +0800

    update error message

--- plan/parse.go
+++ plan/parse.go
@@ -26,7 +26,7 @@ func Parse(version, sql string, explainRows [][]string) (_ Plan, err error) {
 			for _, row := range explainRows {
 				explainContent += strings.Join(row, "\t") + "\n"
 			}
-			err = fmt.Errorf("parse sql=%v ver=%v panic\n, explain: %v\n", sql, version, explainContent)
+			fmt.Printf("parse sql=%v ver=%v panic\n, explain: %v\n\n", sql, version, explainContent)
 			panic(r)
 		}
 	}()
